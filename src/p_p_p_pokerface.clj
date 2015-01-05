51e97337e0aec29e7dbcbf9276d6e1bf2c9f1fd7
Mon Zafra <monzee@gmail.com>
2015-01-05 13:00:44 +0800

    passes all tests

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,47 @@
 (ns p-p-p-pokerface)
 
-(defn rank [card]
-  nil)
+(defn rank [[r _]]
+  (if (Character/isDigit r)
+    (Integer/valueOf (str r))
+    ({\T 10, \J 11, \Q 12, \K 13, \A 14} r)))
 
-(defn suit [card]
-  nil)
+(defn suit [[_ s]]
+  (str s))
+
+(defn count-by [criteria hand]
+  (->> hand (map criteria) (frequencies)))
 
 (defn pair? [hand]
-  nil)
+  (->> hand (count-by rank) (vals) (some #{2}) boolean))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (->> hand (count-by rank) (vals) (some #{3}) boolean))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (->> hand (count-by rank) (vals) (some #{4}) boolean))
 
 (defn flush? [hand]
-  nil)
+  (->> hand (count-by suit) (count) (== 1)))
 
 (defn full-house? [hand]
-  nil)
+  (->> hand (count-by rank) (vals) (every? #{2 3})))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [freqs (->> hand (count-by rank) (vals))]
+    (or (boolean (some #{4} freqs))
+        (== 2 (count (filter #(== 2 %) freqs))))))
 
 (defn straight? [hand]
-  nil)
+  (let [s (set (map rank hand))
+        range (- (apply max s) (apply min s))]
+    (and (== 5 (count s))
+         (or (== 4 range) (== 12 range)))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (some (fn [[check score]] (when (check hand) score))
+        [[straight-flush? 8] [four-of-a-kind? 7]  [full-house? 6] [flush? 5]
+         [straight? 4]       [three-of-a-kind? 3] [two-pairs? 2]  [pair? 1] 
+         [(fn [_] true) 0]]))
